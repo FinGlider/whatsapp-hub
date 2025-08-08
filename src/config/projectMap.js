df3dfc7d9edc4b3902d687a22da700012ffa2eae
Hafis111 <107378152+Hafis111@users.noreply.github.com>
2025-08-08 08:56:10 +0530

    change the phone number id

--- src/config/projectMap.js
+++ src/config/projectMap.js
@@ -1,9 +1,9 @@
 module.exports = {
-  123456789012345: {
+  542491768952983: {
     name: "Appointment System",
     endpoint: "http://localhost:4001/",
   },
-  987654321098765: {
+  542491768952983: {
     name: "WA PROMOTION",
     endpoint: "https://wapromoapi.finglider.com/whatsapp-webhook",
   },
