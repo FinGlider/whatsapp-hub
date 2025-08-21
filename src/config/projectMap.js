d14ae87e1b6493dba54e7bfc4838b392c83fedd1
Hafis111 <107378152+Hafis111@users.noreply.github.com>
2025-08-21 16:12:48 +0530

    added phoneNumberId also

--- src/config/projectMap.js
+++ src/config/projectMap.js
@@ -1,10 +1,8 @@
 module.exports = {
   542491768952983: {
-    name: "Appointment System",
-    endpoint: "http://localhost:4001/",
-  },
-  542491768952983: {
+    phoneNumberId: 542491768952983,
     name: "WA PROMOTION",
     endpoint: "https://wapromoapi.finglider.com/whatsapp-webhook",
   },
+  // Add other phone number IDs here as needed
 };
