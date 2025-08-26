5a1235a89661a025d50d14549cca52b9bec9746a
Hafis111 <107378152+Hafis111@users.noreply.github.com>
2025-08-26 14:31:00 +0530

    fix

--- src/config/projectMap.js
+++ src/config/projectMap.js
@@ -1,12 +1,10 @@
 module.exports = {
   542491768952983: {
-    default: {
-      name: "APPOINTMENT",
-      endpoint: "https://appointmentApi.finglider.com/webhook/whatsapp-webhook",
-    },
-    promo: {
-      name: "WA PROMOTION",
-      endpoint: "https://wapromoapi.finglider.com/whatsapp-webhook",
-    },
+    name: "WA PROMOTION",
+    endpoint: "https://wapromoapi.finglider.com/whatsapp-webhook",
+  },
+  542491768952984: {
+    name: "APPOINTMENT",
+    endpoint: "https://appointmentApi.finglider.com/webhook/whatsapp-webhook",
   },
 };
