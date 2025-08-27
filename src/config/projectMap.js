a5ae457a17bfaa3cff48d4ac3a762a388ff841c6
Hafis111 <107378152+Hafis111@users.noreply.github.com>
2025-08-27 09:02:40 +0530

    WA PROMOTION commended

--- src/config/projectMap.js
+++ src/config/projectMap.js
@@ -1,9 +1,9 @@
 module.exports = {
+  // 542491768952983: {
+  //   name: "WA PROMOTION",
+  //   endpoint: "https://wapromoapi.finglider.com/whatsapp-webhook",
+  // },
   542491768952983: {
-    name: "WA PROMOTION",
-    endpoint: "https://wapromoapi.finglider.com/whatsapp-webhook",
-  },
-  542491768952984: {
     name: "APPOINTMENT",
     endpoint: "https://appointmentApi.finglider.com/webhook/whatsapp-webhook",
   },
